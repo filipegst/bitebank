f9331ff344f453ae2f24702340645eedd1aa973f
Filipe Guimarães <filipegst@live.com>
2023-02-13 07:27:55 -0300

    Criando Repositorio Git

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main (){
+    println ("Bem vindo ao ByteBank !");
+    val  titular = "Filipe"
+    val numeroConta = 1010
+    var saldo = 0.0
+    saldo = 100.0
+    saldo += 200.0
+
+    println("Titular: $titular" )
+    println("Numero da conta: $numeroConta")
+    println("Saldo R$: $saldo")
+
+}

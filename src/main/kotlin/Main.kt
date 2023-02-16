722ff1b249407f874fa879620e1a9e9a37a994bf
Filipe Guimarães <filipegst@live.com>
2023-02-16 15:53:49 -0300

    Criando classe Diretor e convertendo bonificacao para propertie

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,20 +10,39 @@ fun main() {
         salario = 2000.00,
         senha = 1234
     )
+    val diego = Diretor(
+        nome = "Diego",
+        cpf = "333.333.333-33",
+        salario = 3000.00,
+        senha = 9876,
+        plr = 300.00
+    )
+
     println("Funcionario: ${amanda.nome}")
     println("CPF:${amanda.cpf}")
     println("Salario:${amanda.salario}")
-    println("Bonificação:${amanda.bonificacao()}")
+    println("Bonificação:${amanda.bonificacao}")
 
     println()
-    print("Digite a senha:")
-        if (julio.autentica(senha = readln().toInt())) {
-            println("senha correta")
-            println("Gerente: ${julio.nome}")
-            println("CPF:${julio.cpf}")
-            println("Salario:${julio.salario}")
-            println("Bonificação:${julio.bonificacao()}")
-        }
+    print("Digite a senha do Gerente:")
+    if (julio.autentica(senha = readln().toInt())) {
+        println("senha correta")
+        println("Gerente: ${julio.nome}")
+        println("CPF:${julio.cpf}")
+        println("Salario:${julio.salario}")
+        println("Bonificação:${julio.bonificacao}")
     }
+
+    println()
+    print("Digite a senha do Diretor:")
+    if (diego.autentica(senha = readln().toInt())) {
+        println("senha correta")
+        println("Gerente: ${diego.nome}")
+        println("CPF:${diego.cpf}")
+        println("Salario:${diego.salario}")
+        println("Bonificação:${diego.bonificacao}")
+        println("PLR:${diego.plr}")
+    }
+}
 
 

5c97fe431aef82db66977bda01721e242f746189
Filipe Guimarães <filipegst@live.com>
2023-02-16 11:00:01 -0300

    Criando classe Gerente

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,29 @@
 fun main() {
-    testaComportamentoConta()
-}
+    val amanda = Funcionarios(
+        nome = "Amanda",
+        cpf = "111.111.111-11",
+        salario = 1000.00
+    )
+    val julio = Gerente(
+        nome = "Julio",
+        cpf = "222.222.222-22",
+        salario = 2000.00,
+        senha = 1234
+    )
+    println("Funcionario: ${amanda.nome}")
+    println("CPF:${amanda.cpf}")
+    println("Salario:${amanda.salario}")
+    println("Bonificação:${amanda.bonificacao()}")
+
+    println()
+    print("Digite a senha:")
+        if (julio.autentica(senha = readln().toInt())) {
+            println("senha correta")
+            println("Gerente: ${julio.nome}")
+            println("CPF:${julio.cpf}")
+            println("Salario:${julio.salario}")
+            println("Bonificação:${julio.bonificacao()}")
+        }
+    }
 
 

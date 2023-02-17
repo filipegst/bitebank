954c336e6995b0ad4241ef5c7325d97e354c80a6
Filipe Guimarães <filipegst@live.com>
2023-02-17 18:43:00 -0300

    Criando cliente e interfaces
    (codigo não compila no momento

--- src/main/kotlin/testaAutenticacao.kt
+++ src/main/kotlin/testaAutenticacao.kt
@@ -5,8 +5,13 @@ fun testaAutenticacao() {
         salario = 2000.00,
         senha = 1234,
     )
+    val julia = Cliente (nome = "Julia",
+        cpf = "123.456.789-00",
+        senha = 5678)
+
     print("Digite a senha:")
 
     val sistema = SistemaInterno()
     sistema.entra(marco, senha = readln().toInt())
+    julia.autentica(senha = readln().toInt())
 }

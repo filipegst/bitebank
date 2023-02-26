a2451fc7ec76ea0c07d1bfd5c58e10c64b4735f6
Filipe Guimarães <filipegst@live.com>
2023-02-26 08:40:47 -0300

    testando null

--- src/main/kotlin/br/com/bitebank/Main.kt
+++ src/main/kotlin/br/com/bitebank/Main.kt
@@ -1,11 +1,20 @@
 package br.com.bitebank
 
 
+import br.com.bitebank.Modelo.Endereco
 import br.com.bitebank.Testes.testaComportamentoConta
 
 
 fun main() {
-testaComportamentoConta()
+    var enderecoNulo: Endereco? = Endereco("rua nelo de melo", cidade = "rio de maneiro")
+    val ruaNova: String? = enderecoNulo?.rua
+    println(enderecoNulo?.rua)
+    enderecoNulo?.let {
+        println(it.rua.length)
+        val tamanho : Int = it.cidade?.length?: throw IllegalStateException ("cidade não pode ser nulo")
+        println(tamanho)
+    }
+
 }
 
 
--- src/main/kotlin/br/com/bitebank/Modelo/Endereco.kt
+++ src/main/kotlin/br/com/bitebank/Modelo/Endereco.kt
@@ -4,7 +4,7 @@ class Endereco (
     var rua: String = "",
     var numerodoendereco : Int = 0,
     var cep : Int = 0,
-    var cidade : String = "",
+    var cidade : String? = null ,
     var estado: String = "",
     var bairro: String = ""
     )

52aba25dbd0a0ecf3c9d8c268dc24e6cf5002e68
Filipe Guimarães <filipegst@live.com>
2023-02-25 21:37:22 -0300

    Aula lidando com mais de uma exception

--- src/main/kotlin/br/com/bitebank/Main.kt
+++ src/main/kotlin/br/com/bitebank/Main.kt
@@ -1,13 +1,11 @@
 package br.com.bitebank
 
 import br.com.bitebank.Modelo.Conta
+import br.com.bitebank.Testes.testaComportamentoConta
 import br.com.bitebank.Testes.testaContasDiferentes
 
 fun main() {
-    testaContasDiferentes()
-    println()
-    println (Conta.total)
-
+testaComportamentoConta()
 }
 
 
